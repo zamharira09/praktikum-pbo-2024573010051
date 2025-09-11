0268410c18c87b90aa1e73104a3f6ab18082241d
Zamharira <zamharil2391@gmail.com>
2025-09-11 15:42:06 +0700

    inisialisasi 1

---
+++ src/modul_1/Main.java
@@ -0,0 +1,8 @@
+package modul_1;
+
+public class Main {
+    //fungsi khsusus sebagai entry point dari aplikasi yang dibuat.
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
